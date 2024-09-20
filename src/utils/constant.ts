0c9aa451c2371de715d0e8508f5b12b78dcfaad5
Inga Pal <149205377+IngaPal@users.noreply.github.com>
2024-09-20 23:44:53 +0200

    Update constant.ts

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,2 +1,2 @@
 export const base_url: string = 'https://api.openweathermap.org/data/2.5/weather';
-export const api_key: string = 'c662ea6662edbc46f73604ec1a761a09';
+export const api_key: string = 
